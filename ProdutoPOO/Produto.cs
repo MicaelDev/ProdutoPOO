e5fcfb0b134bf810cc6953cd9efb01180b3f8768
Micael20 <74980510+MicaelDev@users.noreply.github.com>
2022-03-16 00:08:36 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutoPOO/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+
+namespace ProdutoPOO
+{
+    class Produto
+    {
+        #region Atributos
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        #endregion
+
+        #region Métodos
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos()
+        {
+
+        }
+        public void RemoverProdutos()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2",CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        #endregion
+    }
+}
